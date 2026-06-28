44aa7bf83935f3e0248e65d092e9ee1e1bf7cf2d
NgThien <nthien390@gmail.com>
2026-06-28 19:51:37 +0000

    Update UI

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const client = axios.create({
-  baseURL: 'https://file.vkanis.xyz/',
+  // baseURL: 'https://file.vkanis.xyz/',
+  baseURL: "http://192.168.1.19:8080/",
   timeout: 30000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
