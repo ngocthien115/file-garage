f712401ca8c9b02ed2a42f3d012f6867957f3f85
NgThien <thien.vu@finfan.vn>
2026-06-29 16:54:50 +0700

    Update eas config

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: 'https://file.vkanis.xyz/',
-  baseURL: "http://192.168.1.19:8080/",
+  baseURL: 'https://file.vkanis.xyz/',
+  // baseURL: "http://192.168.1.19:8080/",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
